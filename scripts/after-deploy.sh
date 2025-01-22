7cc446c1b875382a35adc7fb657364572f34b1d7
hysong4u <hayeon_song@naver.com>
2025-01-22 18:50:12 +0900

    [CHORE] 배포스크립트 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -18,14 +18,4 @@ yarn || { echo "Yarn 설치 또는 실행에 실패했습니다."; exit 1; }
 # pm2 실행
 # pm2 start dist || { echo "PM2 실행에 실패했습니다."; exit 1; }
 
-echo "PM2 프로세스 상태 확인 중..."
-pm2 describe index.js > /dev/null 2>&1
-RUNNING=$?
-
-if [ $RUNNING -ne 0 ]; then
-  echo "PM2에서 index.js가 실행 중이지 않습니다. 새로 시작합니다..."
-  pm2 start index.js || { echo "PM2로 index.js 시작에 실패했습니다."; exit 1; }
-else
-  echo "PM2에서 index.js가 실행 중입니다. 재시작합니다..."
-  pm2 restart index.js || { echo "PM2로 index.js 재시작에 실패했습니다."; exit 1; }
-fi
+pm2 start index.js
